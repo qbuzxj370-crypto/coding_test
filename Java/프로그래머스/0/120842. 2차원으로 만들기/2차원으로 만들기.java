deb1b68cdedfa2ca46fff7a4f7e1a275e73f292c
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-10-16 13:23:10 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int len = num_list.length / n;
+        int[][] answer = new int[len][n];
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < n; j++) {
+                answer[i][j] = num_list[i*n + j];
+            }
+        }
+        return answer;
+    }
+}
