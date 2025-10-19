fe16473669a0d36c695e4d2565ec11cf0c4e9a1e
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-10-19 22:53:11 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order > 0) {
+            int tmp = order % 10;
+            if(tmp % 3 == 0 && tmp != 0) {
+                answer++;
+            }
+            order /= 10;
+        }
+        return answer;
+    }
+}
