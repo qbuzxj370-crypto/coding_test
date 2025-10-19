d3f2c0b6ca69d2988fa6e81a58e5af1024e36ad8
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-10-19 22:46:16 +0900

    [level 0] Title: rny_string, Time: 0.35 ms, Memory: 80.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181863. rny＿string/rny＿string.java
+++ Java/프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String rny_string) {
-        String answer = rny_string.replace("m", "rn");
-        return answer;
+        return rny_string.replaceAll("m", "rn");
     }
 }
