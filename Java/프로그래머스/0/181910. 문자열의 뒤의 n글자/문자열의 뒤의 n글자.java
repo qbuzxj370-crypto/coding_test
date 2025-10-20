d05c0c17399633e825459a25cebf0a2eb1ae5062
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-10-20 12:57:53 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 13.21 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        int start = my_string.length() - n;
+        for(int i = start; i < my_string.length(); i++) {
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
