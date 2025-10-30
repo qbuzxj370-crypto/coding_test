a388e27feb7a03de5cc59de59a5106f4957f0062
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-10-30 11:12:46 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.06 ms, Memory: 73 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (s1, s2) -> s1.charAt(n) - s2.charAt(n) == 0 ? s1.compareTo(s2) : s1.charAt(n) - s2.charAt(n));
+        return strings;
+    }
+    
+}
