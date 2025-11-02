251ef21e2f025363d6444cfbd84c039fd9e6d0b7
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-11-02 23:45:23 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.51 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int max = numbers[numbers.length - 1] * numbers[numbers.length -2];
+        int min = numbers[0] * numbers[1];
+        return max > min ? max : min;
+    }
+}
