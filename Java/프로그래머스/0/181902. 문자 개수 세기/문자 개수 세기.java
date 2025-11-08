b8167fff7bfaaa8abfa77f0c56b005ff67b59e30
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-11-08 23:52:02 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.58 ms, Memory: 81.7 MB -BaekjoonHub

--- Java/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ Java/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,13 +1,8 @@
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        int idx = 0;
         for(char c : my_string.toCharArray()) {
-            idx = c - 65;
-            if(c > 90) {
-                idx -= 6;
-            }
-            answer[idx]++;
+            answer[c - 'A' - (Character.isLowerCase(c) ? 6 : 0)]++;
         }
         return answer;
     }
