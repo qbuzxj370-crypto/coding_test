f5b2db8906be1621adea070c2041b6c137a3b641
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-12-15 13:33:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.22 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        // 참가자 카운트
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        // 완주자 카운트 감소
+        for(String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        // 값이 0이 아닌 사람 = 완주 못한 사람
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) return key;
+        }
+        
+        return "";
+    }
+}
