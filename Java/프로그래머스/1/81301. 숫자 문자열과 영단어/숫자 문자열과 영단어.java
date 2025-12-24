4558848b100ca32f6bd0572eabf01e806d07113c
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-12-24 10:35:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 7.63 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String s) {
+        String[] str = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < str.length; i++) {
+            s = s.replace(str[i], i+"");
+        }
+        return Integer.parseInt(s);
+    }
+}
