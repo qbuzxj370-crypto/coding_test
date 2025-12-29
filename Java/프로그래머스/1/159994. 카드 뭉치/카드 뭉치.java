dc1850ca0204914c57ee3aeb3ad051f250976c3b
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-12-29 13:12:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int idx1 = 0, idx2 = 0;
+
+        for (String g : goal) {
+            if (idx1 < cards1.length && g.equals(cards1[idx1])) {
+                idx1++;
+            } else if (idx2 < cards2.length && g.equals(cards2[idx2])) {
+                idx2++;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+}
