b96a155b8b332dd6374e492a41d0356f0722fde1
SONG SEUNG JUN <qbuzxj370@g.shingu.ac.kr>
2025-12-31 17:16:51 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b) {
+        for(int i = b; i > 0; i--) {
+            if(a % i == 0 && b % i == 0) {
+                a /= i;
+                b /= i;
+                break;
+            }
+        }
+        while(b % 2 == 0) b /= 2;
+        while(b % 5 == 0) b /= 5;
+        return b == 1 ? 1 : 2;
+    }
+}
